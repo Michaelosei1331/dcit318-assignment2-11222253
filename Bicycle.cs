ad8638021540ceff3de32c1fdad5c99084bf7725
Michael Osei Bonsu <bmosei002@st.ug.edu.gh>
2025-08-08 23:03:07 +0000

    Bicycle

---
+++ Bicycle.cs
@@ -0,0 +1,10 @@
+﻿namespace Interfaces
+{
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+}
