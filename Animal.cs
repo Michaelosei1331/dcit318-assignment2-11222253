58232b2f360063610cf056b71b21efa3ffef78d1
Michael Osei Bonsu <bmosei002@st.ug.edu.gh>
2025-08-08 22:39:32 +0000

    Animal Class

---
+++ Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace Inheritance
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+}
